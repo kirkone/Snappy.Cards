8dcf01acb7877907ada8cf3e70d9a0a0f6735eba
Thomas Müller <tmueller@cpan.org>
2022-07-20 11:49:36 +0000

    :tada: Initial commit for new cards

--- src/Cards/src/app.tsx
+++ src/Cards/src/app.tsx
@@ -1,20 +1,59 @@
-import { Logo } from './logo';
+// #region Imports
 
-export function App() {
-    return (
-        <>
-            <Logo />
-            <p>Hello Vite + Preact!</p>
-            <p>
-                <a
-                    class="link"
-                    href="https://preactjs.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn Preact
-                </a>
-            </p>
-        </>
-    );
-}
+import { ElmishResult, Init, Update, cmd } from "@fun-ts/elmish";
+
+import type { PreactView } from "@fun-ts/elmish-preact";
+import { absurd } from "fp-ts/function";
+
+// #endregion
+
+// ============================================================================
+// #region Model
+// ============================================================================
+export type Model = {};
+
+export const init: Init<Model, Msg> = (): ElmishResult<Model, Msg> => [
+    {},
+    cmd.none
+];
+
+// #endregion
+
+// ============================================================================
+// #region Messages
+// ============================================================================
+export type Msg =
+    | { type: "M1"; }
+    | { type: "M2"; }
+    ;
+
+// #endregion
+
+// ============================================================================
+// #region Update
+// ============================================================================
+export const update: Update<Model, Msg> = (model, msg): ElmishResult<Model, Msg> => {
+    switch (msg.type) {
+        case "M1":
+            return [model, cmd.none];
+
+        case "M2":
+            return [model, cmd.none];
+
+        default:
+            return absurd(msg);
+    }
+};
+
+// #endregion
+
+// ============================================================================
+// #region View
+// ============================================================================
+export const view: PreactView<Model, Msg> = (dispatch, _model) => (
+    <>
+        <button onClick={() => dispatch({ type: "M1" })}>-</button>
+    </>
+);
+
+// #endregion
