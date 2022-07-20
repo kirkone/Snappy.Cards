9740ad2a775001a250ce7bf03b876ed4b66f9b28
Thomas Müller <tmueller@cpan.org>
2022-07-20 15:23:16 +0000

    :lipstick: Introduce vanilla extract

--- src/Cards/vite.config.ts
+++ src/Cards/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [
+        preact(),
+        vanillaExtractPlugin(),
+    ],
     esbuild: {
         logOverride: { 'this-is-undefined-in-esm': 'silent' }
     }
