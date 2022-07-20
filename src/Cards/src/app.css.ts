44c99ad795633955a2e96d02b726e82a14c9bdb7
Thomas Müller <tmueller@cpan.org>
2022-07-20 20:34:01 +0000

    :lipstick: Show background if it exists

--- src/Cards/src/app.css.ts
+++ src/Cards/src/app.css.ts
@@ -1,8 +1,16 @@
 import { style } from "@vanilla-extract/css";
 
+export const app = style({
+    height: "100vh",
+    width: "100vw",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+});
+
 export const card = style({
     minHeight: 300,
     paddingBottom: "10rem",
     gap: "2rem",
-    background: "rebeccapurple"
 });
