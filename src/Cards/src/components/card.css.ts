f9b508bd0b226a5de8e151d83b4705503a32129c
Thomas Müller <tmueller@cpan.org>
2022-07-28 21:30:30 +0000

    :sparkles: Add card component

---
+++ src/Cards/src/components/card.css.ts
@@ -0,0 +1,116 @@
+import { style } from "@vanilla-extract/css";
+import { varsApp } from "../theme/variables.css";
+
+const ellipsisLine = {
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+} as const;
+
+export const container = style({
+    borderRadius: varsApp.space.large,
+    maxWidth: "50rem",
+    minWidth: "30rem",
+
+    // bottom layout background is clipped by rounded corners
+    overflow: "hidden",
+});
+
+export const card = style({
+    padding: varsApp.space.large,
+
+    display: "flex",
+    flexWrap: "wrap",
+});
+
+export const layoutNarrow = style({
+    margin: "0 auto", // center after detail breaks to new line
+
+    padding: varsApp.space.medium,
+    paddingBottom: varsApp.space.large,
+});
+
+export const layoutWide = style({
+    flex: 1,
+
+    padding: varsApp.space.medium,
+    paddingBottom: varsApp.space.large,
+
+    // control text overflow
+    maxWidth: "100%",
+});
+
+export const layoutBottom = style({
+    flex: "1 0 100%",
+
+    // padding of card works really well with layoutNarrow and layoutWide
+    // so keep padding of card and work against it here to reach edge of card
+    // 1. first cancel container padding, left, right and bottom
+    margin: `calc(${varsApp.space.large} * -1)`,
+    marginTop: 0,
+
+    // 2. bring back paddings similar to container plus layout{Wide,Narrow}
+    padding: `
+        0
+        calc(${varsApp.space.large} + ${varsApp.space.medium})
+    `,
+
+    backgroundColor: "#0000001a", // TODO: color
+
+    // font
+    color: "rgba(0, 0, 0, 0.09)",  // TODO: color
+    fontWeight: 900,
+    fontSize: "5rem",
+    minHeight: "7rem",
+    textAlign: "center",
+    lineHeight: 7 / 5,
+    ...ellipsisLine,
+});
+
+export const avatarCircle = style({
+    // circle shape
+    width: 120,
+    height: 120,
+    borderRadius: "50%",
+    overflow: "hidden",
+
+    // ring
+    boxShadow: `0 0 0 ${varsApp.space.medium} rgba(${varsApp.color.text.rgb}, 0.1)`,
+    margin: varsApp.space.medium, // box shadow does not change dimensions
+
+    // to center and align contained image
+    position: "relative",
+});
+
+export const avatar = style({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+});
+
+export const detail = style({
+    color: varsApp.color.text.hex,
+    lineHeight: 2,
+});
+
+export const detailHeading = style({
+    fontSize: "2.5rem",
+    fontWeight: 600,
+    lineHeight: 1,
+    paddingBottom: "0.75em",
+    ...ellipsisLine,
+});
+
+export const detailLine = style({
+    // control text overflow
+    ...ellipsisLine,
+});
+
+export const detailIcon = style({
+    verticalAlign: "-18%",
+    marginRight: "0.5em",
+    color: "#ffffffbb", // TODO: color
+});
---
+++ src/Cards/src/components/card.story.tsx
@@ -0,0 +1,36 @@
+import * as A from "fp-ts/Array";
+import * as IO from "fp-ts/IO";
+import * as O from "fp-ts/Option";
+import * as RAND from "fp-ts/Random";
+import * as theme from "../theme/variables.css";
+
+import { fakerD, permute } from "./utils";
+import { flow, pipe } from "fp-ts/function";
+
+import { Card } from "./card";
+
+const getCardData = flow(
+    fakerD,
+    faker => ({
+        name: [O.some(`${faker.name.firstName()} ${faker.name.lastName()}`), O.none],
+        phone: [O.some(faker.phone.number()), O.none],
+        mail: [O.some(faker.internet.email()), O.none],
+        web: [O.some(faker.internet.url()), O.none],
+        sub: [O.some(faker.company.bsBuzz()), O.none],
+        avatar: [O.some(faker.internet.avatar()), O.none],
+    }),
+    permute
+);
+
+const CardStory = () => pipe(
+    RAND.randomInt(0, 200),
+    IO.map(flow(
+        getCardData,
+        A.map(data => <div style={{ margin: 30 }}>
+            <Card data={data} />
+        </div>),
+        (children) => <div class={theme.defaultTheme}>{children}</div>
+    )),
+)();
+
+export default CardStory;
