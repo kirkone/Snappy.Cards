0011865d19deb11d7aac5cbca7411b89868cd50a
Thomas Müller <tmueller@cpan.org>
2022-10-13 12:11:44 +0000

    :lipstick: Give qr code a background in vitebook

--- src/Cards/src/components/qr-code.story.tsx
+++ src/Cards/src/components/qr-code.story.tsx
@@ -1,18 +1,19 @@
 import { QrCode } from "./qr-code";
 import { Variant } from '@vitebook/preact';
+import { background } from "./qr-code.story.css";
 
 const QrCodeStory = () => <>
     <Variant
         name="Default"
         description="QR Code."
     >
-        <QrCode text="1" />
+        <QrCode text="1" className={background} />
     </Variant>
 
     <Variant
         name="Qr Code with border"
         description="Qr Code with border 2.">
-        <QrCode text="1" border={2} />
+        <QrCode text="1" border={2} className={background} />
     </Variant>
 </>;
 
