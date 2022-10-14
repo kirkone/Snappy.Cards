6b97a54cff8944f985bb6a25a477252d52d7bd53
Thomas Müller <tmueller@cpan.org>
2022-10-14 15:46:25 +0000

    :sparkles: Adjust footer height

--- src/Cards/src/components/page.tsx
+++ src/Cards/src/components/page.tsx
@@ -1,20 +1,37 @@
 import * as styles from "./page.css";
 
 import type { FunctionComponent } from "preact";
+import { absurd } from "fp-ts/function";
 import { assignInlineVars } from "@vanilla-extract/dynamic";
+
+type FitHeightTypes = "content" | "screen";
 
 type PageProps = {
     align?: "center" | "start" | "end";
+    fit?: FitHeightTypes;
 };
 
 export const Page: FunctionComponent<PageProps> = ({
     children,
-    align = "center"
+    align = "center",
+    fit = "screen"
 }) => (
     <section
         className={styles.page}
-        style={assignInlineVars({ [styles.CssVarAlignment]: align })}
+        style={assignInlineVars({
+            [styles.CssVarAlignment]: align,
+            [styles.CssVarFit]: translateFitToCss(fit),
+        })}
     >
         {children}
     </section>
 );
+
+const translateFitToCss = (fit: FitHeightTypes) => {
+    switch (fit) {
+        case "content": return "fit-content";
+        case "screen": return "100vh";
+
+        default: return absurd<FitHeightTypes>(fit);
+    }
+};
