102c7d690e35fa680bbcfa1a29f4ab2749f7e03b
Thomas Müller <tmueller@cpan.org>
2022-10-18 22:10:26 +0000

    :truck: Move data functions to model folder

--- src/Cards/src/components/app.tsx
+++ src/Cards/src/components/app.tsx
@@ -1,10 +1,6 @@
 // #region Imports
 
-import * as IO from "fp-ts/IO";
 import * as O from "fp-ts/Option";
-import * as P from "fp-ts/Predicate";
-import * as R from "fp-ts/Record";
-import * as S from "fp-ts/string";
 import * as T from "fp-ts/Task";
 import * as styles from "./app.css";
 import * as theme from "../theme/variables.css";
@@ -12,15 +8,17 @@ import * as theme from "../theme/variables.css";
 import { ADTType, makeADT, ofType } from "@morphic-ts/adt";
 import { Card, CardData } from "./card";
 import { ElmishResult, Init, Update, cmd } from "@fun-ts/elmish";
-import { flow, identity, pipe } from "fp-ts/function";
+import { UrlParameters, getParametersFromUrl } from "../model/url-data";
 
-import type { Endomorphism } from "fp-ts/Endomorphism";
 import { Footer } from "./footer";
 import { Page } from "./page";
 import type { PreactView } from "@fun-ts/elmish-preact";
 import { QrCodeCard } from "./qr-code-card";
 import { assignInlineVars } from "@vanilla-extract/dynamic";
+import { getCardData } from "../model/card-data";
+import { getVCardUrl } from "../model/v-card-url";
 import { makeRemoteResultADT } from "@fun-ts/remote-result-adt";
+import { pipe } from "fp-ts/function";
 
 // #endregion
 
@@ -51,7 +49,10 @@ export const init: Init<Model, Msg> = (): ElmishResult<Model, Msg> => [
 // #region Messages
 // ============================================================================
 type GetUrlDataMsg = { type: "GetUrlData"; };
-type GetUrlDataSucceededMsg = { type: "GetUrlDataSucceeded"; data: UrlData; };
+type GetUrlDataSucceededMsg = {
+    type: "GetUrlDataSucceeded";
+    data: UrlParameters;
+};
 
 type GetCurrentUrlMsg = { type: "GetCurrentUrl"; };
 type GetCurrentUrlSucceededMsg = {
@@ -94,7 +95,7 @@ export const update: Update<Model, Msg> = (model, msg) => pipe(
             {
                 ...model,
                 appData: pipe(
-                    decodeUrlParameters(data),
+                    getCardData(data),
                     ({ background, ...card }) => AppDataAdt.as.Loaded({
                         card,
                         background,
@@ -165,111 +166,6 @@ export const view: PreactView<Model, Msg> = (_dispatch, model) => (
 // #endregion
 
 // ============================================================================
-// #region Data from URL
-// ============================================================================
-type KnownUrlParameters =
-    | "name"
-    | "phone"
-    | "mail"
-    | "web"
-    | "sub"
-    | "avatar"
-    | "background";
-
-const getParametersFromInput = (
-    input: string
-) => pipe(
-    input,
-    O.fromPredicate(P.not(S.isEmpty)),
-    O.map(flow(
-        s => [...new URLSearchParams(s).entries()],
-        R.fromEntries,
-    ))
-);
-
-const getParametersFromSearch = pipe(
-    IO.of(document.location.search.substring(1)),
-    IO.map(getParametersFromInput),
-);
-
-const getParametersFromHash = pipe(
-    IO.of(document.location.hash.substring(1)),
-    IO.map(getParametersFromInput),
-);
-
-const getParametersFromUrl = pipe(
-    getParametersFromSearch,
-    IO.map(flow(
-        O.alt(getParametersFromHash),
-        O.getOrElseW((): Record<string, string> => ({}))
-    )),
-);
-
-type UrlData = ReturnType<typeof getParametersFromUrl>;
-// #endregion
-
-// ============================================================================
-// #region Decode parameters
-// ============================================================================
-type ImageParams =
-    | "random"
-    | `http${string}`
-    | (string & {});
-
-type UrlParamDecoder = Endomorphism<O.Option<string>>;
-
-const StringParamDecoder: UrlParamDecoder = identity;
-
-type UnsplashParams = {
-    width: number;
-    height: number;
-    params: Record<string, string>;
-};
-
-const ImageParamDecoder = (
-    { width, height, params }: UnsplashParams
-) => (
-    input: O.Option<ImageParams>
-) => pipe(
-    input,
-    O.map(i => i.startsWith("http") ?
-        i :
-        `https://source.unsplash.com/${i}/${width}x${height}?${new URLSearchParams(params)}`
-    ),
-);
-
-const urlParameters: Record<KnownUrlParameters, UrlParamDecoder> = {
-    name: StringParamDecoder,
-    phone: StringParamDecoder,
-    mail: StringParamDecoder,
-    web: StringParamDecoder,
-    sub: StringParamDecoder,
-    avatar: ImageParamDecoder({
-        width: 100,
-        height: 100,
-        params: { face: "" }
-    }),
-    background: ImageParamDecoder({
-        width: 1000,
-        height: 1000,
-        params: { blur: "" }
-    }),
-};
-
-const decodeUrlParameters = (
-    params: Record<string, string>
-): Record<KnownUrlParameters, O.Option<string>> => pipe(
-    urlParameters,
-    R.mapWithIndex((name, decode) => pipe(
-        params,
-        R.lookup(name),
-        decode,
-    )),
-);
-
-// #endregion
-
-// ============================================================================
 // #region ADT for cardData
 // ============================================================================
 const AppDataAdt = makeRemoteResultADT<
@@ -301,54 +197,3 @@ const QrCodeView = CurrentUrlAdt.matchStrict({
     Failure: () => <>⚠ An error occurred while retrieving current URL ⚠</>,
     Loaded: ({ href }) => <QrCodeCard href={href} />
 });
-
-// #endregion
-
-// ============================================================================
-// #region VCard
-// ============================================================================
-type VCardFields = Exclude<
-    KnownUrlParameters,
-    | "sub"
-    | "avatar"
-    | "background"
->;
-
-const VCardParamDecoder = (prefix: string) => (param: O.Option<string>) => pipe(
-    param,
-    O.map(p => `${prefix}${p}\n`)
-);
-
-const vCardParameters: Record<VCardFields, UrlParamDecoder> = {
-    name: VCardParamDecoder("N:"),
-    phone: VCardParamDecoder("TEL;TYPE=PREF:"),
-    mail: VCardParamDecoder("EMAIL;TYPE=PREF,INTERNET:"),
-    web: VCardParamDecoder("URL:"),
-};
-
-const encodeVCardFields = (params: Record<string, string>) => pipe(
-    vCardParameters,
-    R.mapWithIndex((name, decode) => pipe(
-        params,
-        R.lookup(name),
-        decode,
-    )),
-);
-
-const renderVCard = (params: Record<VCardFields, O.Option<string>>) => pipe(
-    params,
-    R.foldMap(S.Ord)(S.Monoid)(O.getOrElse(() => "")),
-    content => `BEGIN:VCARD\nVERSION:3.0\n${content}END:VCARD`
-);
-
-const vCardUrl = (vcardData: string) => pipe(
-    new Blob([vcardData], { type: "text/vcard" }),
-    URL.createObjectURL,
-);
-
-const getVCardUrl = flow(
-    encodeVCardFields,
-    renderVCard,
-    vCardUrl,
-);
-// #endregion
