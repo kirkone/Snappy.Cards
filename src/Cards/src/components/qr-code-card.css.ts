9070b1c644301020f64242cbef4411c35481d13e
Thomas Müller <tmueller@cpan.org>
2022-10-19 12:04:38 +0000

    :sparkles: Add support for light mode

--- src/Cards/src/components/qr-code-card.css.ts
+++ src/Cards/src/components/qr-code-card.css.ts
@@ -11,14 +11,15 @@ export const qrCode = style({
     borderRadius: varsApp.space.small,
 
     animation: `${fadeIn} 1s`,
-    backgroundColor: `rgb(${varsApp.color.dark.rgb},.75)`,
-    transition: "background 1s",
+    backgroundColor: `rgb(${varsApp.color.light.rgb},.75)`,
+    color: varsApp.color.dark.hex,
+    transition: "background 1s, color 1s",
 
     boxShadow: `7px 7px 15px 0px rgb(${varsApp.color.shadow.rgb}, 0.4) inset`,
 
     selectors: {
         "&:hover": {
-            backgroundColor: varsApp.color.dark.hex,
+            backgroundColor: varsApp.color.light.hex,
         }
     }
 });
