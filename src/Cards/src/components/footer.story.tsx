09fda0a0ae547cdfe9ba537310f1332260e93085
Thomas Müller <tmueller@cpan.org>
2022-10-22 22:10:49 +0000

    :sparkles: Implement name as vCard filename

--- src/Cards/src/components/footer.story.tsx
+++ src/Cards/src/components/footer.story.tsx
@@ -1,18 +1,32 @@
+import * as O from "fp-ts/Option";
+
 import { Footer } from "./footer";
 import { VCardDataAdt } from "../model/v-card-url";
 
 const QrCodeCardStory = () => <>
     <h1 style={{ backgroundColor: "white" }}>Loaded</h1>
-    <Footer downloadUrl={VCardDataAdt.as.Loaded({ url: "" })}></Footer>
+    <Footer
+        name={O.some("Guybrush Threepwood")}
+        downloadUrl={VCardDataAdt.as.Loaded({ url: "" })}
+    />
 
     <h1 style={{ backgroundColor: "white" }}>NotLoaded</h1>
-    <Footer downloadUrl={VCardDataAdt.as.NotLoaded({ url: "" })}></Footer>
+    <Footer
+        name={O.some("Guybrush Threepwood")}
+        downloadUrl={VCardDataAdt.as.NotLoaded({ url: "" })}
+    />
 
     <h1 style={{ backgroundColor: "white" }}>Failure</h1>
-    <Footer downloadUrl={VCardDataAdt.as.Failure({ url: "" })}></Footer>
+    <Footer
+        name={O.some("Guybrush Threepwood")}
+        downloadUrl={VCardDataAdt.as.Failure({ url: "" })}
+    />
 
     <h1 style={{ backgroundColor: "white" }}>Loading</h1>
-    <Footer downloadUrl={VCardDataAdt.as.Loading({ url: "" })}></Footer>
+    <Footer
+        name={O.some("Guybrush Threepwood")}
+        downloadUrl={VCardDataAdt.as.Loading({ url: "" })}
+    />
 </>;
 
 export default QrCodeCardStory;
--- src/Cards/src/utils/utils.tsx
+++ src/Cards/src/utils/utils.tsx
@@ -1,7 +1,9 @@
+import { curry2, memoize as memoizeF } from "fp-ts-std/Function";
+import { flip, flow } from "fp-ts/function";
+
 import type { Eq } from "fp-ts/Eq";
 import type { Task } from "fp-ts/Task";
-import { flow } from "fp-ts/function";
-import { memoize as memoizeF } from "fp-ts-std/Function";
+import filenamify from "filenamify";
 import { memoize as memoizeIO } from "fp-ts-std/IO";
 
 export const memoizeTask = memoizeIO as <A>(f: Task<A>) => Task<A>;
@@ -10,3 +12,8 @@ export const memoizeTaskK =
     <A,>(eq: Eq<A>) =>
         <B,>(f: (x: A) => Task<B>) =>
             memoizeF(eq)(flow(f, memoizeTask));
+
+export const sanitizeFilename = curry2(flip(filenamify))({
+    replacement: "_",
+    maxLength: 200
+});
