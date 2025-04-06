8cd11a5847233933a40a85b36a101c1cc4e2389a
Thomas Müller <tmueller@cpan.org>
2025-04-06 22:06:51 +0000

    :arrow_up: Fix npm audit issues

--- src/Cards/src/components/qr-code.story.tsx
+++ src/Cards/src/components/qr-code.story.tsx
@@ -1,19 +1,19 @@
-import { QrCode } from "./qr-code";
-import { Variant } from '@vitebook/preact';
+// import { QrCode } from "./qr-code";
+// import { Variant } from '@vitebook/preact';
 
-const QrCodeStory = () => <>
-    <Variant
-        name="Default"
-        description="QR Code."
-    >
-        <QrCode text="1" />
-    </Variant>
+// const QrCodeStory = () => <>
+//     <Variant
+//         name="Default"
+//         description="QR Code."
+//     >
+//         <QrCode text="1" />
+//     </Variant>
 
-    <Variant
-        name="Qr Code with border"
-        description="Qr Code with border 2.">
-        <QrCode text="1" border={2} />
-    </Variant>
-</>;
+//     <Variant
+//         name="Qr Code with border"
+//         description="Qr Code with border 2.">
+//         <QrCode text="1" border={2} />
+//     </Variant>
+// </>;
 
-export default QrCodeStory;
+// export default QrCodeStory;
