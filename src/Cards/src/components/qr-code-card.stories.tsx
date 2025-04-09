5b1be49847ced027be4b83aab69c49891da7676a
Thomas Müller <tmueller@cpan.org>
2025-04-09 21:34:00 +0000

    :sparkles: Add story for qr-code-card component

---
+++ src/Cards/src/components/qr-code-card.stories.tsx
@@ -0,0 +1,32 @@
+import { flow, pipe } from "fp-ts/function";
+import * as IO from "fp-ts/IO";
+import * as RAND from "fp-ts/Random";
+
+import { Meta, StoryObj } from "../utils/storybook";
+import { fakerD } from "../utils/utils-vitebook";
+import { QrCodeCard } from "./qr-code-card";
+
+const meta = {
+    title: "Components/QrCodeCard",
+    component: QrCodeCard,
+    args: {
+    },
+} satisfies Meta<typeof QrCodeCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const getQrCodeCardData = flow(
+    fakerD,
+    faker => faker.internet.url(),
+);
+
+export const Default: Story = {
+    args: {
+        href: pipe(
+            RAND.randomInt(0, 200),
+            IO.map(getQrCodeCardData),
+        )()
+    },
+};
